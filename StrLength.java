f07233b403a61301a0f6b6ddf95709e5660d9be6
sunilganotra <sunil.ganotra@gmail.com>
2025-01-13 23:14:06 +0530

    Create StrLength.java

---
+++ StrLength.java
@@ -0,0 +1,6 @@
+public class StrLength {
+    public static void main(String[] args) { //This code prints the length of the string
+        String Name = "The quick brown fox jumps";
+        System.out.println("Length of the String is: " + Name.length());
+    }
+}
