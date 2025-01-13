2e0cdaf2034f56848f54cf369b180657758f8ce5
sunilganotra <sunil.ganotra@gmail.com>
2025-01-13 22:53:46 +0530

    Create ConcatStr.java

---
+++ ConcatStr.java
@@ -0,0 +1,11 @@
+
+public class ConcatStr {
+    public static void main(String[] args) { //This function joins strings
+        String s1 = "AKARSH";
+        String s2 = "JHA";
+        String s3 = s1.concat(s2);
+        String s4 = s1 + " " + s2;
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
