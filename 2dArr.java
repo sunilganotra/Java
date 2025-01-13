f1d42324f111ff2a2c082486bd7b9855a3d3f12b
sunilganotra <sunil.ganotra@gmail.com>
2025-01-13 23:09:02 +0530

    Create 2dArr.java

---
+++ 2dArr.java
@@ -0,0 +1,14 @@
+//This code represents simple 2d array
+
+public class 2dArr {
+    public static void main(String args[]){
+        int marks[][]= {{100,99,98},{98,99,100}};
+        System.out.println(marks[0][0]);
+        System.out.println(marks[0][1]);
+        System.out.println(marks[0][2]);
+        System.out.println(marks[1][0]);
+        System.out.println(marks[1][1]);
+        System.out.println(marks[1][2]);
+    }
+    
+}
